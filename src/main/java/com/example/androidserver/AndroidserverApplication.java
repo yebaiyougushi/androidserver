ffbdeffcc2e749bdbb95f5f2a5264e0eb22395c5
23520055542@qq.com <2352005542@qq.com>
2020-08-30 15:47:57 +0800

    test android servcer

--- src/main/java/com/example/androidserver/AndroidserverApplication.java
+++ src/main/java/com/example/androidserver/AndroidserverApplication.java
@@ -8,6 +8,8 @@ public class AndroidserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AndroidserverApplication.class, args);
+        //test spring
+
     }
 
 }
